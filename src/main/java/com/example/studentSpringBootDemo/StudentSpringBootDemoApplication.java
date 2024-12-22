7986c7cde75902edbae12f181b3bf4993b002afe
junevision <junevision@163.com>
2024-12-22 11:27:12 +0800

    use Student module as object and generate Json Object into API

--- src/main/java/com/example/studentSpringBootDemo/StudentSpringBootDemoApplication.java
+++ src/main/java/com/example/studentSpringBootDemo/StudentSpringBootDemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.studentSpringBootDemo;
 
+import com.example.studentSpringBootDemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,12 +14,12 @@ import java.util.List;
 @RestController
 public class StudentSpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentSpringBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSpringBootDemoApplication.class, args);
+    }
 
-	@GetMapping
-	public List<String> hello() {
-		return Arrays.asList("Hello", "World");
-	}
+    @GetMapping
+    public List<Student> hello() {
+        return Arrays.asList(new Student(1L, "Jun", "jun@test.com", LocalDate.of(2000, 1, 5), 24));
+    }
 }
