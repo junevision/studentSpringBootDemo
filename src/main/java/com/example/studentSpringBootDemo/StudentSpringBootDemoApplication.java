2aa7b8786e59a0bdfefe5236ad1d1303f4f53922
junevision <junevision@163.com>
2024-12-23 12:00:07 +0800

    create controller and service for student

--- src/main/java/com/example/studentSpringBootDemo/StudentSpringBootDemoApplication.java
+++ src/main/java/com/example/studentSpringBootDemo/StudentSpringBootDemoApplication.java
@@ -1,25 +1,13 @@
 package com.example.studentSpringBootDemo;
 
-import com.example.studentSpringBootDemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class StudentSpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentSpringBootDemoApplication.class, args);
-    }
-
-    @GetMapping
-    public List<Student> hello() {
-        return Arrays.asList(new Student(1L, "Jun", "jun@test.com", LocalDate.of(2000, 1, 5), 24));
     }
 }
---
+++ src/main/java/com/example/studentSpringBootDemo/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.studentSpringBootDemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author jun.lei
+ * @created 23/12/2024 - 11:32
+ * @description
+ */
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+}
