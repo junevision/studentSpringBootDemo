6880a5bc0c7334a89fba3b85def4f53ecd358174
junevision <junevision@163.com>
2025-01-02 12:31:59 +0800

    use Transient for field age to auto calculate

--- src/main/java/com/example/studentSpringBootDemo/student/Student.java
+++ src/main/java/com/example/studentSpringBootDemo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.studentSpringBootDemo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * @author jun.lei
@@ -27,24 +28,23 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dateOfBirth;
+    @Transient
     private Integer age;
 
     public Student() {
     }
 
-    public Student(Long id, String name, String email, LocalDate dateOfBirth, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dateOfBirth) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dateOfBirth = dateOfBirth;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dateOfBirth, Integer age) {
+    public Student(String name, String email, LocalDate dateOfBirth) {
         this.name = name;
         this.email = email;
         this.dateOfBirth = dateOfBirth;
-        this.age = age;
     }
 
     public Long getId() {
@@ -80,7 +80,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dateOfBirth, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
